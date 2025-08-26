3cd51eaedae8f6b20fcbacc33d1a0fb3142dcfd2
azu-25 <azuazuroy2000@gmail.com>
2025-08-26 14:20:59 +0100

    My first commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import random
 
+number = random.randint(1, 20)
+print("I am thinking of a number between 1 and 20.")
+
+while True:
+    guess = int(input("Take a guess:"))
+    if guess == number:
+        print("correct!")
+        break
+    elif guess < number:
+        print("too low")
+    else:
+        print("too high")
